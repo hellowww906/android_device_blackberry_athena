4dfd3fe29e6c72dde94e163b714034d15f4a7bbf
hellow <rhiyu01@gmail.com>
2026-09-22 13:58:02 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fox_athena.mk
+    $(LOCAL_DIR)/twrp_athena.mk
 
 COMMON_LUNCH_CHOICES := \
-    fox_athena-user \
-    fox_athena-userdebug \
-    fox_athena-eng
+    twrp_athena-user \
+    twrp_athena-userdebug \
+    twrp_athena-eng
 
